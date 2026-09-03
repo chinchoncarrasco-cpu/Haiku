217c679b499d4132be60e718dbf0af9a3aa3114f
chinchoncarrasco-cpu <chinchon.carrasco@gmail.com>
2026-09-02 22:14:26 -0400

    Habilitar fotos en revisión para todas las cabañas

--- js/supabase-cabanas-fotos-v1.js
+++ js/supabase-cabanas-fotos-v1.js
@@ -8,7 +8,7 @@
     "use strict";
 
     const CABANAS_CON_FOTO = new Set([
-        "1", "2", "3", "4", "6"
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"
     ]);
 
     function obtenerRevision() {
